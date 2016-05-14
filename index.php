c265c5dbc76c2a8b5f242eb5b7740ff1ef57d790
LucasSales <lucasalesvieira2@gmail.com>
2016-05-14 19:17:36 -0300

    TrabalhoFeito

--- index.php
+++ index.php
@@ -1,15 +1,21 @@
 <?php
     include_once "Conversor.php";
-
-    if(isset($_POST['valor'])){
+    if(isset($_GET['valor'])){
         $con = new Conversor();
-        $valor = $_POST['valor'];
-        $atual = $_POST['atual'];
-        $convertido = $_POST['convertido'];
+        $valor = $_GET['valor'];
+        $atual = $_GET['atual'];
+        $convertido = $_GET['convertido'];
 
         $a = $con->converter($valor,$atual,$convertido);
 
         echo $a;
+    }else if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $con = new Conversor();
+        $json = json_decode(file_get_contents('php://input'),true);
+        $a = $con->converter($json["valor"],$json["moedaAtual"],$json["moedaConvertido"]);
+        $json["valorConvertido"] = $a;
+        echo json_encode($json);
+        exit;
     }
 ?>
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
@@ -19,7 +25,7 @@
     <title>Document</title>
 </head>
 <body>
-<form action="index.php" method="post">
+<form action="index.php" method="get">
     <input type="text" name="valor">
     <select name="atual">
         <option value="Real">Real</option>
--- testeJson.php
+++ testeJson.php
@@ -6,19 +6,19 @@
  * Time: 14:42
  */
 
-$url  = 'http://ec2-52-37-59-164.us-west-2.compute.amazonaws.com:3000/calculator';
+$url  = 'http://localhost/Cloud/index.php';
 
-$data = ["type" => "multiply","firstValue" => 3,"secondValue"=>2];
+$data = ["moedaAtual" => "Real","moedaConvertido" => "Dolar","valor"=>4];
 $data = json_encode($data);
 
 $ch   = curl_init();
 curl_setopt($ch,CURLOPT_HTTPHEADER,['Content-Type: application/json']);
 curl_setopt($ch, CURLOPT_URL, $url);
 curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+//curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $data);
 
 $result = curl_exec($ch);
 $resultado = json_decode($result);
-echo $resultado->result;
+//echo $resultado;
 
